0f11317071d3ef24089a1bb04526ae68a75bab1e
Gaziz Islamov <pazos@bk.ru>
2023-04-26 20:01:16 +0300

    через date-fns упростил функций для определения периода. Вместо indexOf использовал поиск URL

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import * as dateFns from "https://cdn.skypack.dev/date-fns";
+
 let man = [
   { name: "Robert", precent: 2, summ: 213000 },
   { name: "Dima", precent: 23 / 100, summ: 214 },
@@ -57,78 +59,51 @@ function bonusCreation() {
   }
 }
 
-// let arr = [
-//   { month: 00, quarter: 1 },
-//   { month: 01, quarter: 1 },
-//   { month: 02, quarter: 1 },
-//   { month: 03, quarter: 2 },
-//   { month: 04, quarter: 2 },
-//   { month: 05, quarter: 2 },
-//   { month: 06, quarter: 3 },
-//   { month: 07, quarter: 3 },
-//   { month: 08, quarter: 3 },
-//   { month: 09, quarter: 4 },
-//   { month: 10, quarter: 4 },
-//   { month: 11, quarter: 4 },
-// ];
-
 let monthsWithQuoter = {
-  00: 1,
-  01: 1,
-  02: 1,
-  03: 2,
-  04: 2,
-  05: 2,
-  06: 3,
-  07: 3,
-  08: 3,
-  09: 4,
+  0: 1,
+  1: 1,
+  2: 1,
+  3: 2,
+  4: 2,
+  5: 2,
+  6: 3,
+  7: 3,
+  8: 3,
+  9: 4,
   10: 4,
   11: 4,
 };
 
 let out3 = document.querySelector(".out3");
-let strDatefrom;
-let strDateto;
-let resultUrl = decodeURIComponent(location.search.substring(1)).split("&");
 
-for (let i = 0; i < resultUrl.length; i++) {
-  let indexDateFrom = resultUrl[i].indexOf("date_from");
-  let indexDateto = resultUrl[i].indexOf("date_to");
+let resultUrl2 = window.location.href;
 
-  if (indexDateFrom != -1) {
-    out3.innerHTML = `${resultUrl[i]} <br/  >`;
-    strDatefrom = resultUrl[i];
-  }
-  if (indexDateto != -1) {
-    out3.innerHTML += resultUrl[i];
-    strDateto = resultUrl[i];
-  }
-}
+const url = new URL(resultUrl2);
+
+let strDatefrom = url.searchParams.get("date_from");
+let strDateto = url.searchParams.get("date_to");
+console.log(strDatefrom);
 
 // что подаем на вход:
 //  arrDatefrom;
 // что возвращаем  - return quarter
+
 function findMonthQuarter(date) {
-  let arrSansEqualDF = date.split("=");
-  let itemDate = arrSansEqualDF[1];
-  let arrDataWithoutDot = itemDate.split(".");
+  let arrDataWithoutDot = date.split(".");
   let [day, month, year] = arrDataWithoutDot;
   let dateDayMonthYear = new Date(year, month, day);
-  console.log();
-  console.log(dateDayMonthYear.getDay());
-
   let findQuarter =
     monthsWithQuoter[
       dateDayMonthYear.getMonth()
     ]; /*  Это тоже самое, что и  let monthItemDT = arr.find((item) => item.month == Dateto.getMonth());
-                                                                                         console.log(monthItemDT.quarter);   */
+                                                                                       console.log(monthItemDT.quarter);   */
   return findQuarter;
 }
 
 let quaterFrom = findMonthQuarter(strDatefrom);
 let quarterTo = findMonthQuarter(strDateto);
 console.log(quaterFrom);
+console.log(quarterTo);
 
 // (man[i].summ / 100) * 1(man[i].summ / 100) * 1;
 
@@ -143,72 +118,48 @@ console.log(quaterFrom);
 //   }
 // }
 // test2();
-let day;
-const arrWeek = ["Пн", "Вт", "Ср", "Чт", "Пт", "Cб", "Вс"];
+
+const period = url.searchParams.get("period");
 let today = new Date();
 
 /* День */
-for (let i = 0; i < resultUrl.length; i++) {
-  let indexToday = resultUrl[i].indexOf("period=day");
-  if (indexToday != -1) {
-    day = today;
+function periodDay() {
+  if (period === "day") {
+    strDatefrom = today;
+    strDateto = today;
   }
 }
 /* День */
-
-/* неделя */ function week() {
-  let DateFrom = new Date();
-  let DateDoto = new Date();
-  let newFrom;
-  let newDOto;
-  for (let i = 0; i < resultUrl.length; i++) {
-    let indexWeek = resultUrl[i].indexOf("period=week");
-    if (indexWeek != -1) {
-      let dayOfWeek = today.getDay();
-      let dayPassedWeek = arrWeek.length - dayOfWeek;
-      DateFrom.setDate(DateFrom.getDate() - (dayOfWeek - 1));
-      newFrom = DateFrom;
-      DateDoto.setDate(DateDoto.getDate() + dayPassedWeek);
-      newDOto = DateDoto;
-    }
+/* Прошлый День */
+function yesterday() {
+  if (period === "yesterday") {
+    strDatefrom = dateFns.sub(today, { days: 1 });
+    strDateto = strDatefrom;
   }
-  console.log(newFrom);
-  console.log(newDOto);
 }
-week();
+yesterday();
+/* Прошлый День */
 /* неделя */
-/* месяц */
-
-function month() {
-  let monthDateDoto = new Date();
-  let monthDateFrom = new Date();
-
-  let samplebyYear = monthDateDoto.getFullYear();
-  let samplebyMonth = monthDateDoto.getMonth();
-  let samplebyDay = monthDateDoto.getDate();
-  // let firstdayMonth = getDate();
-  // console.log(firstdayMonth);
-
-  function getLastDayOfMonth(year, month) {
-    let date = new Date(year, month + 1, 0);
-    return date.getDate();
+function startEndWeek() {
+  if (period === "week") {
+    const startEndWeekUS = dateFns.startOfWeek(today);
+    strDatefrom = dateFns.add(startEndWeekUS, { days: 1 });
+    const lastDayWeekUS = dateFns.lastDayOfWeek(today);
+    strDateto = dateFns.add(lastDayWeekUS, { days: 1 });
   }
+}
+startEndWeek();
 
-  let dotoMonth = getLastDayOfMonth(samplebyYear, samplebyMonth);
-  monthDateDoto.setDate(monthDateDoto.getDate() + (dotoMonth - samplebyDay));
-  monthDateFrom.setDate(monthDateFrom.getDate() - (samplebyDay - 1));
-  console.log(monthDateDoto);
-  console.log(monthDateFrom);
+/* неделя */
+
+/* месяц */
+function month() {
+  if (period === "month") {
+    const lastDayMonth = dateFns.lastDayOfMonth(today);
+    const firstDayMonth = dateFns.startOfMonth(today);
+    strDatefrom = firstDayMonth;
+    strDateto = lastDayMonth;
+  }
 }
 month();
 /* месяц */
-
-{
-  /* <script>
-  function dni() {
-    var D = new Date(1999,11,31);
-    D.setDate(D.getDate() + 3);
-    alert(D);
-  }
-  </script> */
-}
