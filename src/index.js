fbfc8ed111537258685bf77649429aeef5e10b03
Gaziz Islamov <pazos@bk.ru>
2023-05-09 12:27:35 +0300

    дорабатал таблицу, теперь данные запрашиваются с бекенда и выводятся по периодам

--- src/index.js
+++ src/index.js
@@ -9,59 +9,6 @@ import {
   url,
 } from "./helpers.js";
 
-let man = [
-  { index: 1, name: "Robert", precent: 2, summ: 213000 },
-  { index: 2, name: "Dima", precent: 23 / 100, summ: 214 },
-  { index: 3, name: "Gaziz", precent: 24, summ: 215 },
-  { index: 4, name: "Riba", precent: 27, summ: 215 },
-];
-
-let precent;
-let thead = document.querySelector(".js-thead");
-let tbody = document.querySelector(".js-tbody");
-let table = document.querySelector(".js-table");
-
-function thCreation(classlist, text) {
-  let th = document.createElement("th");
-  th.scope = classlist;
-  th.innerText = text;
-  return th;
-}
-function theadAppend() {
-  let theadFragment = new DocumentFragment();
-  thead.append(thCreation("col", "#"));
-  thead.append(thCreation("col", "Name"));
-  thead.append(thCreation("col", "Precent"));
-  thead.append(thCreation("col", "Summ"));
-  theadFragment.append(thead);
-  return theadFragment;
-}
-
-table.append(theadAppend());
-
-function tbodyCreation() {
-  man.forEach((element) => {
-    // let fragment = new DocumentFragment();
-    let tr = document.createElement("tr");
-
-    let thFirst = document.createElement("th");
-    tr.append(thFirst);
-    thFirst.scope = "row";
-    thFirst.innerHTML = element.index;
-    let tdSecond = document.createElement("td");
-    tr.append(tdSecond);
-    tdSecond.innerHTML = element.name;
-    let tdThird = document.createElement("td");
-    tr.append(tdThird);
-    tdThird.innerHTML = element.precent;
-    let tdForth = document.createElement("td");
-    tr.append(tdForth);
-    tdForth.innerHTML = element.summ;
-    tbody.append(tr);
-  });
-}
-tbodyCreation();
-
 let monthsWithQuoter = {
   0: 1,
   1: 1,
@@ -77,7 +24,7 @@ let monthsWithQuoter = {
   11: 4,
 };
 
-let out3 = document.querySelector(".out3");
+// let out3 = document.querySelector(".out3");
 
 // что подаем на вход:
 //  arrDatefrom;
@@ -111,33 +58,268 @@ console.log(quarterTo);
 //     let test4 = (test3.innerHTML / 100) * 1;
 //     console.log(test4);
 //   }
-// }
+// };
 // test2();
+
 const period = url.searchParams.get("period");
-/* День */
-if (period === "day") {
-  console.log(periodDay());
+const needSendDate = url.searchParams.get("sendDate") === "true";
+
+let postDataFilterDate = {
+  type: "filter_leads",
+  data: {
+    from: "",
+    to: "",
+    manager_id: "",
+  },
+};
+
+function getDateFrom() {
+  if (period == "day") {
+    return periodDay().timeFromTimestamp;
+  }
+  if (period == "yesterday") {
+    return yesterday().yesterdayFromTimestamp;
+  }
+  if (period == "week") {
+    return startEndWeek().timestampweekFrom;
+  }
+  if (period == "month") {
+    return month().timestampMonthFrom;
+  }
+  if (period == "custom") {
+    let parsestrDatefrom = dateFns.parse(strDatefrom, "dd.MM.yyyy", new Date());
+    let dateFrom = parsestrDatefrom.getTime();
+    return dateFrom;
+  }
 }
 
-/* День */
+function getDateTo() {
+  if (period == "day") {
+    return periodDay().timeToTimestamp;
+  }
+  if (period == "yesterday") {
+    return yesterday().yesterdayToTimestamp;
+  }
+  if (period == "week") {
+    return startEndWeek().timestampWeekTo;
+  }
+  if (period == "month") {
+    return month().timestampMonthTo;
+  }
+  if (period == "custom") {
+    let parsestrDateTo = dateFns.parse(strDateto, "dd.MM.yyyy", new Date());
+    let dateTo = parsestrDateTo.getTime();
 
-/* Прошлый День */
-if (period === "yesterday") {
-  console.log(yesterday());
+    return dateTo;
+  }
+}
+function getDateFromFormatted() {
+  if (period == "day") {
+    return periodDay().timeFrom;
+  }
+  if (period == "yesterday") {
+    return yesterday().yesterdayFrom;
+  }
+  if (period == "week") {
+    return startEndWeek().startEndWeekFrom;
+  }
+  if (period == "month") {
+    return month().monthFrom;
+  }
+  if (period == "custom") {
+    let parsestrDatefrom = dateFns.parse(strDatefrom, "dd.MM.yyyy", new Date());
+    let dateFrom = parsestrDatefrom;
+    return dateFrom;
+  }
+}
+function getDateToFormatted() {
+  if (period == "day") {
+    return periodDay().timeTo;
+  }
+  if (period == "yesterday") {
+    return yesterday().yesterdayTo;
+  }
+  if (period == "week") {
+    return startEndWeek().startEndWeekTo;
+  }
+  if (period == "month") {
+    return month().monthTo;
+  }
+  if (period == "custom") {
+    let parsestrDateTo = dateFns.parse(strDateto, "dd.MM.yyyy", new Date());
+
+    let dateTo = parsestrDateTo;
+
+    return dateTo;
+  }
 }
 
-/* Прошлый День */
+let filterDateParams = {
+  from: getDateFrom(),
+  to: getDateTo(),
+  fromFormatted: getDateFromFormatted(),
+  toFormatted: getDateToFormatted(),
+};
 
-/* неделя */
-if (period === "week") {
-  console.log(startEndWeek());
+console.log(filterDateParams);
+
+let postDataManagers = {
+  type: "managers",
+  data: {},
+};
+
+function loadManagers() {
+  const queryParams = {
+    type: "managers",
+  };
+
+  if (needSendDate) {
+    queryParams.data = filterDateParams;
+    console.log((queryParams.data = filterDateParams));
+  }
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://app.aaccent.su/jenyanour/",
+      type: "post",
+      data: queryParams,
+
+      dataType: "json", // Expected response data type
+
+      success: function (data) {
+        console.log(data);
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
+    });
+  });
+}
+function loadLeads(managerId) {
+  const queryParams = {
+    type: "filter_leads",
+  };
+
+  if (needSendDate) {
+    queryParams.data = managerId;
+    console.log((queryParams.data = managerId));
+    queryParams.data = filterDateParams;
+    queryParams.data.manager_id = managerId;
+  }
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://app.aaccent.su/jenyanour/",
+      type: "POST",
+
+      data: queryParams,
+
+      dataType: "json", // Expected response data type
+      success: function (data) {
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
+    });
+  });
 }
 
-/* неделя */
+function loadLeadsComplete(managerId) {
+  const queryParams = {
+    type: "filter_complete_leads",
+  };
 
-/* месяц */
-if (period === "month") {
-  console.log(month());
+  if (needSendDate) {
+    queryParams.data = filterDateParams;
+    queryParams.data.manager_id = managerId;
+  }
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://app.aaccent.su/jenyanour/",
+      type: "post",
+      data: queryParams,
+
+      dataType: "json", // Expected response data type
+      success: function (data) {
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
+    });
+  });
 }
+function renderManagers(managersArr) {
+  const fragment = $(document.createDocumentFragment());
 
-/* месяц */
+  managersArr.forEach((manager, index) => {
+    let totalSumm = 0;
+    let totalSummFormatted;
+    const allLeadsCount = manager.leads.length + manager.leadsCompleted.length;
+    const percentOfCompletedLeads = allLeadsCount
+      ? Math.round((manager.leadsCompleted.length * 100) / allLeadsCount)
+      : 0;
+    // console.log(percentOfCompletedLeads);
+    const $row = $("<tr>");
+    const $index = $("<td>").text(index + 1);
+    const $name = $("<td>").text(manager.name);
+    const $percentActiveLeads = $("<td>").text(percentOfCompletedLeads + "%");
+    manager.leadsCompleted.map((item, index) => {
+      totalSumm += Number(item.lead_sum);
+      totalSummFormatted = totalSumm.toLocaleString("en-US");
+    });
+
+    const $summ = $("<td>").text(totalSummFormatted);
+    function bonusQuarter() {
+      if (quarterTo == quaterFrom && percentOfCompletedLeads < 9) {
+        return $("<td>").text(0);
+      }
+      if (
+        percentOfCompletedLeads >= 10 &&
+        quarterTo == quaterFrom &&
+        percentOfCompletedLeads < 27
+      ) {
+        const tenPercent = $("<td>").text(
+          Math.round((totalSumm / 100) * 1).toLocaleString("en-US")
+        );
+        return tenPercent;
+      }
+      if (
+        percentOfCompletedLeads >= 27 &&
+        quarterTo == quaterFrom &&
+        percentOfCompletedLeads < 100
+      ) {
+        const twentySevenPrecent = $("<td>").text(
+          Math.round((totalSumm / 100) * 2.7).toLocaleString("en-US")
+        );
+        return twentySevenPrecent;
+      }
+    }
+
+    $row
+      .append($index)
+      .append($name)
+      // .append($percentActiveLeads)
+      // .append($summ)
+      .append(bonusQuarter());
+    fragment.append($row);
+  });
+  $("table tbody").append(fragment);
+}
+async function render() {
+  const managers = await loadManagers();
+
+  const newManagerListWithLeads = managers.map(async (item) => {
+    const leads = await loadLeads(item.id);
+    const leadsCompleted = await loadLeadsComplete(item.id);
+
+    return { ...item, leads, leadsCompleted };
+  });
+  const result = await Promise.all(newManagerListWithLeads);
+
+  console.log(result);
+  renderManagers(result);
+}
+render();
